a0594b7995bf9919e268fa766a1c8096d554f156
Deels2024 <160591824+Deels2024@users.noreply.github.com>
2026-09-01 20:33:30 +0300

    chore(pwa): refresh admin management cache

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const CACHE='owner-property-shell-v3.2.1';
-const SHELL=['/','/app.css?v=3.2.1','/app.js?v=3.2.1','/manifest.webmanifest','/icon.svg'];
+const CACHE='owner-property-shell-v3.3.0';
+const SHELL=['/','/app.css?v=3.3.0','/app.js?v=3.3.0','/manifest.webmanifest','/icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener('fetch',e=>{
